b9936e3d922b3bb74d81f40a07852e079ab270fa
Travis Kochel <travis@scribbletone.com>
2016-03-12 14:01:44 -0800

    base install

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var { Component } = React;
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <h1>Hi</h1>
+    );
+  }
+})
+
+module.exports = App;
